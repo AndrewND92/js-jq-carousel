3c20f39b9301d196c36ec9a1cac21c5d0c62093a
Andrea <miranda.andrea.nd@gmail.com>
2020-07-03 16:46:46 +0200

    immagini e icone con il next

--- main.js
+++ main.js
@@ -4,12 +4,28 @@ $(document).ready(function(){
   // variabili delle  frecce avanti e indietro
 
   nextBtn.click(function(){
+    // variabile per le immagini e icone con classe active
+
+    // immagini
     var imgActive = $('.slider-wrapper img.active');
      imgActive.removeClass('active');
-     imgActive.next().addClass('active');
+     // icone
+     var iActive = $('.slider-wrapper i.active');
+      iActive.removeClass('active');
+
+
+
+     if (imgActive.hasClass('last')) {
+       $('.slider-wrapper img.first').addClass('active');
+       $('.slider-wrapper i.first').addClass('active');
+
+     }else {
+       imgActive.next().addClass('active');
+       iActive.next().addClass('active');
+     }
   });
 
 
 
 
-  });
+});
